61460d0bf7fa52c3c6cf104abe6177de0b6ed1d2
Einar Sønju <einarsonju@gmail.com>
2024-02-22 19:03:05 +0100

    Implement gradient descent with backprop

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,9 @@ use ndarray::{Array2, ArrayView1, ArrayView2, array, s,  Axis};
 
 const BATCH_SIZE: usize = 50;
 const N_INPUTS: usize = 784;
+const LEARNING_RATE: f32 = 0.005;
 
-
-fn create_network() -> NeuralNetwork {
-    println!("Initializing network");
-
-    let layers: Vec<usize> = vec!(64, 32, 10);
-
-    NeuralNetwork::new(layers, N_INPUTS)
-}
-
+const REPORT_FREQ: usize = 30; // Report info during training every # batches
 
 
 fn cross_entropy_loss(a: ArrayView2<f32>, y: ArrayView2<f32>) -> f32 {
@@ -36,9 +29,9 @@ fn cross_entropy_loss(a: ArrayView2<f32>, y: ArrayView2<f32>) -> f32 {
 }
 
 
-fn train_step(model: &NeuralNetwork, x_batch: ArrayView2<f32>, y_batch: ArrayView2<f32>) -> f32 {
+fn train_step(model: &mut NeuralNetwork, x_batch: ArrayView2<f32>, y_batch: ArrayView2<f32>) -> f32 {
     /*
-     * Perform a training step
+     * Perform a training step, including forward, backward, and gradient descent.
      *
      * X: [bs, 784] array of images to train on
      * Y: [bs, 10] array of labels corresponding to each image
@@ -46,10 +39,17 @@ fn train_step(model: &NeuralNetwork, x_batch: ArrayView2<f32>, y_batch: ArrayVie
     let outputs = model.forward(x_batch);
     assert!(outputs.shape() == [BATCH_SIZE,10]);
 
+    let grads = model.backward(x_batch, y_batch, outputs.view());
+
+    // Gradient descent, using the gradients to update the weights
+    for i in 0..grads.len() {
+        model.W[i] = &model.W[i] - LEARNING_RATE * &grads[i];
+    }
+
     cross_entropy_loss(outputs.view(), y_batch)
 }
 
-fn train(model: NeuralNetwork, dataset: (Array2<f32>, Array2<f32>)) {
+fn train(mut model: NeuralNetwork, dataset: (Array2<f32>, Array2<f32>)) {
     println!("Training..");
 
     let img_array = dataset.0;
@@ -66,21 +66,27 @@ fn train(model: NeuralNetwork, dataset: (Array2<f32>, Array2<f32>)) {
         let x_batch = img_array.slice(s![i..i+BATCH_SIZE,..]);
         let y_batch = labels.slice(s![i..i+BATCH_SIZE,..]);
 
-        let loss: f32 = train_step(&model, x_batch, y_batch);
+        let loss: f32 = train_step(&mut model, x_batch, y_batch);
 
-        println!("{}", loss);
 
         its+= 1;
-        if its % 120 == 0 {
+        if its % REPORT_FREQ == 0 {
             let elapsed = now.elapsed().as_secs_f32();
 
-            println!("{}/{} \t - {} its/s ", its, its_per_epoch, 120.0/elapsed);
+            println!("Loss: {}\t\t  {} its/s  - \t {}/{}", loss, (REPORT_FREQ as f32)/elapsed, its, its_per_epoch);
 
             now = Instant::now();
         }
     }
 }
 
+fn create_network() -> NeuralNetwork {
+    println!("Initializing network");
+
+    let layers: Vec<usize> = vec!(64, 32, 10);
+
+    NeuralNetwork::new(layers, N_INPUTS)
+}
 
 fn main() {
     // The dataset consists of a [60000, 784] array containing the image data,
--- src/network.rs
+++ src/network.rs
@@ -3,27 +3,38 @@ use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Uniform;
 
 pub struct NeuralNetwork {
-    pub W: Vec<Array2<f32>>
+    pub W: Vec<Array2<f32>>, // Weights of the network
+    pub A: Vec<Array2<f32>>, // Hidden layer output
 }
 
 impl NeuralNetwork {
     pub fn new(layers: Vec<usize>, n_inputs: usize) -> NeuralNetwork{
 
         let mut weights: Vec<Array2<f32>> = vec![];
+        let mut activations: Vec<Array2<f32>> = vec![];
 
+        // Initialize empty activation matrices
+        for i in 0..3 {
+            activations.push(Array2::zeros((0,0)));
+        }
+
+        // Initialize weights
         let mut prev: usize = n_inputs;
         for l in layers {
             let shape = (prev, l);
+            println!("{:?}", shape);
             weights.push(Array::random(shape, Uniform::new(-1.,1.))); // TODO: currently initializing uniformly; change to gaussian?
+
             prev = l;
         }
 
         NeuralNetwork {
-            W: weights
+            W: weights,
+            A: activations,
         }
     }
 
-    pub fn forward(&self, X: ArrayView2<f32>) -> Array2<f32> {
+    pub fn forward(&mut self, X: ArrayView2<f32>) -> Array2<f32> {
         /*
          * Forward input through the network
          * X: Array2: [bs, 784]
@@ -31,15 +42,45 @@ impl NeuralNetwork {
 
         let mut l1 = X.dot(&self.W[0]);  // [bs, 64]
         l1 = l1.mapv(|x| sigmoid(x));
+        *&mut self.A[0] = l1.clone();
 
         let mut l2 = l1.dot(&self.W[1]); // [bs, 32]
         l2 = l2.mapv(|x| sigmoid(x));
+        *&mut self.A[1] = l2.clone();
 
         let mut l3 = l2.dot(&self.W[2]); // [bs, 10]
-
         let out = softmax(&l3);
 
         out
+    }
+
+    pub fn backward(&self, X: ArrayView2<f32>, Y: ArrayView2<f32>, outputs: ArrayView2<f32>) -> Vec<Array2<f32>> {
+        /*
+         * Perform a backward pass through the network that calculates the gradients
+         * by backpropagation.
+         *
+         * X:           [bs, 784]
+         * Y:           [bs, 10]
+         * outputs:     [bs, 10]
+         */
+
+        let bs: f32 = X.shape()[0] as f32;
+
+        // Error in the output layer
+        let output_error = &outputs - &Y; // [bs, 10]
+
+        // Errors in hidden layers
+        let l2_error = &output_error.dot(&self.W[2].t()) * (&self.A[1] * (1.0-&self.A[1]));
+        let l1_error = &l2_error.dot(&self.W[1].t()) * (&self.A[0] * (1.0-&self.A[0]));
+
+        // Calculate gradients
+        let mut grads: Vec<Array2<f32>> = vec![];
+
+        grads.push((X.t().dot(&l1_error))/bs);
+        grads.push((self.A[0].t().dot(&l2_error))/bs);
+        grads.push((self.A[1].t().dot(&output_error))/bs);
+
+        grads
     }
 }
 
